1572da1bee6ce318329c0ded3fd9370a62a25bc0
wijaya-felicia <wijaya.felicia.angel@gmail.com>
2025-06-17 16:53:34 +0700

    commit 1

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ from app import app
 class TestApp(unittest.TestCase):
     def test_home(self):
         response = app.test_client().get('/')
-        self.assertEqual(response.data, b'Hello World')
+        self.assertEqual(response.data, b'Hello World!')
 
 if __name__ == "__main__":
     unittest.main()
